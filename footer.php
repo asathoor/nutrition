f0293be0adb5bc0bbd284a327044615c9ac8e968
asathoor <asathoor@gmail.com>
2017-09-10 04:11:25 +0200

    menu

--- footer.php
+++ footer.php
@@ -1,2 +1,7 @@
-</body>
+<!-- file: footer.php -->	
+		<div>
+			<?php include_once "sidebar.php"; ?> 
+		</div>
+	
+	</body>
 </html>
---
+++ sidebar.php
@@ -0,0 +1,8 @@
+<div id="sidebar">
+	<ul id="menu">
+		<li class="menuItem"><a href="http://localhost/php/csv/nutrition/dagbog-chart.php">Track Calories</a></li>
+		<li class="menuItem"><a href="http://localhost/php/csv/nutrition/dagbog-chartgauge.php">Gauge Track Calories</a></li>
+		<li class="menuItem"><a href="http://localhost/php/csv/nutrition/seekerform.php">Kalorietabel (danish)</a></li>
+		<li class="menuItem"><a href="http://localhost/php/csv/nutrition/dagbog.php" target="_blank">Nutrition Diary</a></li>
+	</ul>
+</div>
